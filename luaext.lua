2b0ade3b2cc4c3c68e165903a415698035040a38
NiteHawk <n1tehawk@users.noreply.github.com>
2016-02-04 14:58:56 +0100

    Update documentation

--- luaext.lua
+++ luaext.lua
@@ -1,6 +1,40 @@
+--[[--
+The luaext core module.
+
+
+Usually all you need to do is a
+	require("luaext")
+from your project. With the default [configuration](#_config), this creates a
+global variable `luaext` (holding the module table), and also includes the
+various submodules.
+
+If you prefer less 'intrusive' operation, you may change the configuration
+accordingly, and use something like
+	local luaext = require("luaext")
+Submodules are always available within the module table, so e.g. to access the
+mathematical functions you can use `luaext.math`, etc.
+
+---
+@module luaext
+]]
 local _M = {} -- the module table
 
--- luaext configuration - predefine LUAEXT_CONFIG or edit these settings
+--[[--
+luaext configuration - predefine `LUAEXT_CONFIG` or edit these settings
+
+@field EXPORT_MODULE_AS_GLOBAL
+&nbsp;if set (`true`), luaext will export the module table as global var "luaext"
+
+@field EXPORT_GLOBAL_FUNCTIONS
+&nbsp;if set, luaext will export some function names to the global namespace
+(e.g. `printf`, `error_fmt`, ...)
+
+@field USE_LUA_NAMESPACES
+&nbsp;if set, luaext (submodules) will use / add to predefined Lua namespaces
+(like `math`, `string`, `table`, ...)
+
+@table _config
+]]
 _M._config = rawget(_G, "LUAEXT_CONFIG") or {
 	EXPORT_MODULE_AS_GLOBAL = true,
 	EXPORT_GLOBAL_FUNCTIONS = true,
